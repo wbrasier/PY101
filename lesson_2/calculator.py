fb70056dee2ff890da118e729d69398224b59869
wbrasier <whit.brasier@gmail.com>
2024-06-05 21:03:56 +0000

    refactored calculator, version 2

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -4,23 +4,50 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f'=> {message}')
+
+# Checks to see if the input is an integer
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
 
 # Ask the user for the first number
-number1 = input("What's the first number?\n")
-number2 = input("What's the second number?\n")
+prompt("What's the first number?")
+number1 = input()
 
-print('What operation would you like to perform?\n'
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+number2 = input("=> What's the second number?\n")
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt('What operation would you like to perform?\n'
       '1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation =='1': # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # '2' represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # '3' represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # '4' represents division
-    output = int(number1) / int(number2)
+while operation not in ['1', '2', '3', '4']:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
 
-print(f"The result is: {output}")
+match operation:
+    case '1': # '1' represents addition
+        output = int(number1) + int(number2)
+    case '2': # '2' represents subtraction
+        output = int(number1) - int(number2)
+    case '3': # '3' represents multiplication
+        output= int(number1) * int(number2)
+    case '4': # '4' represents division
+        output = int(number1) / int(number2)
+
+prompt(f"The result is: {output}")
